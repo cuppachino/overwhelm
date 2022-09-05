a68b687b40f07bc6acb9bf6f68baea41ba03162a
Cuppachino <92656523+Cuppachino@users.noreply.github.com>
2022-09-04 22:03:00 -0500

    fix((esbuild)): silence false warning @babel/plugin-transform-react-jsx
    fixes https://github.com/vitejs/vite/issues/8644

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  // workaround https://github.com/vitejs/vite/issues/8644
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+});
