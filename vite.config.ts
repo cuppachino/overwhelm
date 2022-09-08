be3998b4f82444eca094ee991358ded6290b2e0a
Cuppachino <92656523+Cuppachino@users.noreply.github.com>
2022-09-08 12:25:26 -0500

    chore: add node_modules alias "~"

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './node_modules'),
+    },
+  },
   plugins: [react()],
   // workaround https://github.com/vitejs/vite/issues/8644
   esbuild: {
